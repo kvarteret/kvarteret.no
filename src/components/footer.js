7fe679531e2ec8f4874f63f0a29c289f6e76bb87
Jorgen Solum <j-solum@live.com>
2020-10-13 18:56:26 +0200

    footer changes
    footer forced bottom

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,23 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import q from 'qjuul'
 
 import { Link } from 'gatsby'
 
 
 
-import './footer.scss'
+///import './footer.scss'
 
 const Footer = ({ siteTitle }) => {
 
   return (
-    <div style={{ 
-        alignSelf: 'stretch', 
-        height:"10vw", 
-        backgroundColor:"Gray",
-        }}>
-      
-    </div>
+    <q.div frbc po='absolute' bo='0px' w100>
+      <q.div frbc fsb w100>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+      </q.div>
+    </q.div>
   )
 }
 
