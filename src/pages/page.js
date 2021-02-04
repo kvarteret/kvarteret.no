9b9c04e5c1ca4bfdfa522781031348378b8fbb01
wuw012 <56112778+wuw012@users.noreply.github.com>
2021-02-04 19:20:00 +0100

    nmade new page.js

---
+++ src/pages/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const Page = () => {
+
+  return(
+    <Layout>
+    </Layout>
+  )
+}
+
+export default Page
