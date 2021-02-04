b30663aba207d9679d2084ea28eceb2bcccea68c
wuw012 <56112778+wuw012@users.noreply.github.com>
2021-02-04 20:08:02 +0100

    new component pictureDescription

---
+++ src/components/pictureDescription.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Divider, Grid, GridList, GridListTile, makeStyles, Typography } from '@material-ui/core';
+
+const PictureDescription = () => {
+    return (
+        <Grid container>
+            <Grid item>
+                <img src="https://photos.smugmug.com/photos/i-N3ZBZDf/1/X3/i-N3ZBZDf-X3.jpg"/>
+            </Grid>
+            
+        </Grid>
+        
+
+    )
+}
+
+export default PictureDescription;
+
--- src/pages/page.js
+++ src/pages/page.js
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import { Box, Divider, Grid, GridList, GridListTile, makeStyles, Typography } from '@material-ui/core'
 import Layout from '../components/layout'
-import Image from '../components/image'
+import PictureDescription from '../components/pictureDescription'
+
 
 const Page = () => {
-
+  
   return(
     <Layout>
+        <Grid container direction="column">
+            <Grid item xs={12} >
+                <PictureDescription />
+            </Grid>
+            <Grid item xs={12}>
+                <h3>Heyehi</h3>
+            </Grid>
+        </Grid>
     </Layout>
   )
 }
