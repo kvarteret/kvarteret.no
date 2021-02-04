806a0db5c2a6e6e56a250acffa8721e25d0389cd
millianapia <millianapia@gmail.com>
2021-02-04 19:26:33 +0100

    Group page framework

---
+++ src/pages/groups.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Group from "../components/Groups/Group/Group"
+const groupPage = () => {
+
+  return(
+    <Layout>
+      <Group/>
+    </Layout>
+  )
+}
+
+export default groupPage
