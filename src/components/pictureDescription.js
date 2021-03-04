082e2ce99d6fed6caeb2c44b285d1a788cfa2eab
wuw012 <56112778+wuw012@users.noreply.github.com>
2021-03-04 19:45:02 +0100

    Page PictureDescription get ready for props

--- src/components/pictureDescription.js
+++ src/components/pictureDescription.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React from 'react'
+import { Grid } from '@material-ui/core'
 
 const style_image = {
-    height: "100%",
-    width: "100%"
-  };
-
-const PictureDescription = () => {
-    return (
-        <Grid container>
-            <Grid item xs={12}>
-                <img style={style_image} src="https://photos.smugmug.com/photos/i-N3ZBZDf/1/X3/i-N3ZBZDf-X3.jpg"/>
-            </Grid>
-            <Grid item xs={12}> 
-                <Grid container direction="row" justify="space-between" alignItems="center" >
-                    <Grid item>Etasje: </Grid>
-                    <Grid item>Kapasitet: * </Grid>
-                    <Grid item>Bar: </Grid>
-                </Grid>
-            </Grid>
-                <Grid item xs={12}>
-                    <p>* Kapasitet kan variere</p>
-                </Grid>
-        </Grid>
-        
-
-    )
+  height: '100%',
+  width: '100%',
 }
 
-export default PictureDescription;
+const PictureDescription = props => {
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <img
+          style={style_image}
+          src="https://photos.smugmug.com/photos/i-N3ZBZDf/1/X3/i-N3ZBZDf-X3.jpg"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Grid item>Etasje: {props.floor}</Grid>
+          <Grid item>Kapasitet: * </Grid>
+          <Grid item>Bar: </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <p>* Kapasitet kan variere</p>
+      </Grid>
+    </Grid>
+  )
+}
 
+export default PictureDescription
