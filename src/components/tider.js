a9d06d4438c013f1a7e2fce8735abc678e3756ef
TheAzack9 <TheAzack9@users.noreply.github.com>
2021-03-30 16:58:58 +0200

    Added moment + implemented opening time calculation

--- src/components/tider.js
+++ src/components/tider.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 
 import './footer.scss'
 import { Box, Divider, Grid, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
+import moment from 'moment'
 
 const useStyles = makeStyles({
   title: {
@@ -26,11 +27,99 @@ const useStyles = makeStyles({
     fontSize: 12,
     marginTop: 4,
     color: '#929292',
+    textTransform: 'uppercase',
   },
 })
 
+const extractData = (data) => {
+  let dates = {}
+  var openingTimes = data.directus.items.opening_time
+  openingTimes.forEach((item) => {
+    let times = []
+    item.opening_times.forEach((timeobj) => {
+      var time = timeobj.opening_time_day_id
+      times.push({
+        openingTime: time.opening_time,
+        closingTime: time.closing_time,
+        roomName: time.room.name,
+        open: true,
+      })
+    })
+    dates[item.day] = times
+  })
+  return dates
+}
+
+const dateLookup = [
+  'søndag',
+  'mandag',
+  'tirsdag',
+  'onsdag',
+  'torsdag',
+  'fredag',
+  'lørdag',
+]
+
+const currentDay = () => dateLookup[new Date().getDay()]
+
+const RoomTime = ({ classes, time }) => {
+  let timeStr = 'CLOSED'
+  if (time.openingTime && time.closingTime && time.open) {
+    const opening = moment(time.openingTime, 'HH:mm:ss').format('HH:mm')
+    const closing = moment(time.closingTime, 'HH:mm:ss').format('HH:mm')
+    timeStr = opening + ' - ' + closing
+  }
+
+  return (
+    <Grid
+      item
+      container
+      direction="row"
+      justify="space-between"
+      alignItems="center"
+    >
+      <Grid item>
+        <Typography variant="h6" className={classes.text}>
+          {time.roomName}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="h6" className={classes.textTitle}>
+          {timeStr}
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
 const Tider = ({ siteTitle }) => {
   const classes = useStyles()
+
+  const data = useStaticQuery(graphql`
+    query OpeningTimeQuery {
+      directus {
+        items {
+          opening_time {
+            day
+            opening_times {
+              opening_time_day_id {
+                opening_time
+                closing_time
+                room {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  var date = extractData(data)[currentDay()]
+  const dateComponents = date.map((time, index) => (
+    <RoomTime key={index} time={time} classes={classes}></RoomTime>
+  ))
   return (
     <Box>
       <Typography variant="h2" className={classes.title}>
@@ -38,96 +127,7 @@ const Tider = ({ siteTitle }) => {
       </Typography>
       <Divider className={classes.divider}></Divider>
       <Grid container direction="column">
-        <Grid item>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h6" className={classes.text}>
-                STJERNESALEN
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="h6" className={classes.textTitle}>
-                11:30 - 20:00
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h6" className={classes.text}>
-                GRØNDAHLS
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="h6" className={classes.textTitle}>
-                19:30 - 00:30
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h6" className={classes.text}>
-                TEGLVERKET
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="h6" className={classes.textTitle}>
-                21:00 - 00:30
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h6" className={classes.text}>
-                HALVTIMEN
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="h6" className={classes.textTitle}>
-                21:00 - 00:30
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <Grid item>
-              <Typography variant="h6" className={classes.textTitle}>
-                ÅPENT VED ARRANGEMENT
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
+        {dateComponents}
       </Grid>
     </Box>
   )
