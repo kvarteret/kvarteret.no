98b912591ae5fc15d0c07bd4deb6fb6e114c02d6
Obentou <60257518+Obentou-git@users.noreply.github.com>
2021-04-13 01:28:33 +0200

    Update grouppages.js
    Queryen trenger litt puss og kjærleik.

--- src/page_generators/grouppages.js
+++ src/page_generators/grouppages.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const { isValidStatus } = require('../helpers/helper')
+
 module.exports.generate = async (createPage, graphql, actions) => {
   const response = await graphql(`
     query GroupItems {
@@ -26,4 +29,30 @@ module.exports.generate = async (createPage, graphql, actions) => {
       }
     }
   `)
+
+  const {
+    data: { directus: groupObjects },
+  } = response
+  await Promise.all(
+    groupObjects.group.map(async (groupItem) => {
+      if (!isValidStatus(groupItem.status)) return
+
+      return Promise.all(
+        groupItem.translations.map(async (translation) => {
+          let languageModifier = translation.languages_code.url_code + '/'
+
+          const dataContext = {
+            body: translation.description,
+            gallery: groupItem.gallery,
+          }
+
+          createPage({
+            path: '/' + languageModifier + 'group/' + groupItem.slug,
+            component: path.resolve('./src/templates/page.js'),
+            context: dataContext,
+          })
+        })
+      )
+    })
+  )
 }
