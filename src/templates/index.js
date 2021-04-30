8ebba8c81874146ffc3f726a99da08dd0d34eff4
Robin Grundvåg <robin@cavachi.be>
2021-04-30 16:41:31 +0200

    Fix mistake in previous commit

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,13 +4,13 @@ import Layout from '../components/shared/layout/layout'
 import MainContent from '../components/mainSection/mainContent'
 import { useMediaQuery, useTheme } from '@material-ui/core'
 
-const IndexPage = () => {
+const IndexPage = ({ pageContext }) => {
   const theme = useTheme()
   const spacingTop =
     pageContext?.gallery?.length == 0 ||
     useMediaQuery(theme.breakpoints.up('md'))
   return (
-    <Layout spacingTop={spacingTop}>
+    <Layout spacingTop={!spacingTop}>
       <MainContent />
     </Layout>
   )
