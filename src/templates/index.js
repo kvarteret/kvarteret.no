b8b68b6802bf4dfb67f309f65301a22a17d4ee1c
Robin Grundvåg <robin@cavachi.be>
2021-04-30 16:37:50 +0200

    Add spacing to top of index page

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 import Layout from '../components/shared/layout/layout'
 import MainContent from '../components/mainSection/mainContent'
+import { useMediaQuery, useTheme } from '@material-ui/core'
 
 const IndexPage = () => {
+  const theme = useTheme()
+  const spacingTop =
+    pageContext?.gallery?.length == 0 ||
+    useMediaQuery(theme.breakpoints.up('md'))
   return (
-    <Layout>
+    <Layout spacingTop={spacingTop}>
       <MainContent />
     </Layout>
   )
