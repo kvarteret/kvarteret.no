8a2c8d17c66d2fbef01ddf0956af3696af543cc6
Robin Grundvåg <robin@cavachi.be>
2021-05-08 21:20:20 +0200

    [#9] Finalize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,20 +2,30 @@ import React from 'react'
 
 import Layout from '../components/shared/layout/layout'
 import SEO from '../components/seo'
-import { Paper, Grid, Typography, darken } from '@material-ui/core'
+import {
+  Paper,
+  Grid,
+  Typography,
+  darken,
+  Box,
+  Link,
+  Button,
+} from '@material-ui/core'
 import BgImg from '../images/sad_penguin.jpg'
 import { relativeTimeRounding } from 'moment'
+import { graphql, StaticQuery, useStaticQuery } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import { UrlLanguageCode } from '../helpers/languageHelper'
+import { getTranslatedText } from '../helpers/textHelper'
 
 const styles = {
   backgroundContainer: {
-    height: '100vh',
-    width: '100vw',
-    backgroundImage: `url(${BgImg})`,
-    padding: '0',
-    margin: '0',
-    backgroundSize: 'cover',
-    backgroundColor: '#F54B4B',
-    backgroundBlendMode: 'multiply',
+    gridArea: '1/1',
+    mixBlendMode: 'multiply',
+  },
+  otherContainer: {
+    gridArea: '1/1',
+    position: 'relative',
   },
   boxStyle: {
     width: '130%',
@@ -27,36 +37,75 @@ const styles = {
   },
 }
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <Paper style={styles.backgroundContainer}>
-      <Grid
-        style={styles.boxStyleRow}
-        container
-        direction="row"
-        justify="flex-end"
-        alignItems="center"
-      >
-        <Grid
-          style={styles.boxStyle}
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-        >
-          <Typography variant="h1">404</Typography>
-          <Typography variant="h6">
-            Pingvinen kunne ikke finne siden!
-          </Typography>
-          <Typography variant="body1">
-            Men frykt ikke! Du kan hjelpe han å finne veien tilbake ved å klikke
-            knappen under.
-          </Typography>
-        </Grid>
-      </Grid>
-    </Paper>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return (
+    <Layout spacingTop={false}>
+      <SEO title="404" />
+      <div style={{ display: 'grid', backgroundColor: '#f54B4B' }}>
+        <StaticImage
+          src="../images/sad_penguin.jpg"
+          placeholder="blurred"
+          blurredOptions={{ width: 50 }}
+          style={styles.backgroundContainer}
+        />
+        <Box style={styles.otherContainer}>
+          <Grid container direction="row" alignItems="stretch">
+            <Grid item md={6} xs={12}></Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              container
+              direction="column"
+              alignItems="center"
+              justify="center"
+              style={{
+                margin: 'auto',
+                height: '100vh',
+                color: 'white',
+                textAlign: 'center',
+              }}
+            >
+              <Box m={2}>
+                <Grid item>
+                  <Typography variant="h1" style={{ fontSize: 150 }}>
+                    404
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6" style={{ fontSize: 28 }}>
+                    {getTranslatedText('404-title')}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    {getTranslatedText('404-description')}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Box mt={4}>
+                    <Button
+                      component={Link}
+                      href={'/' + UrlLanguageCode()}
+                      underline="none"
+                      variant="contained"
+                      fullWidth="true"
+                      color="white"
+                      style={{ height: 55 }}
+                    >
+                      <Typography variant="h6" color="primary">
+                        {getTranslatedText('go-back-to-main-page')}
+                      </Typography>
+                    </Button>
+                  </Box>
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
