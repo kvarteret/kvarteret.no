b8d6f463cf265992b1f10532874ae4a1f5df02b7
Adrian <loggavdenne@gmail.com>
2021-05-10 14:11:20 +0200

    404 work
    mangler styling på button

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ import {
   Paper,
   Grid,
   Typography,
+<<<<<<< Updated upstream
   darken,
   Box,
   Link,
@@ -17,6 +18,34 @@ import { graphql, StaticQuery, useStaticQuery } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import { UrlLanguageCode } from '../helpers/languageHelper'
 import { getTranslatedText } from '../helpers/textHelper'
+=======
+  Button,
+  ThemeProvider,
+} from '@material-ui/core'
+import BgImg from '../images/sad_penguin.jpg'
+
+const theme = {
+  typography: {
+    h1: {
+      fontSize: '120px',
+      color: 'white',
+      fontWeight: '600',
+      letterSpacing: '2px',
+    },
+    h6: {
+      fontSize: '18px',
+      color: 'white',
+      fontWeight: '500',
+      letterSpacing: '1px',
+    },
+    body1: {
+      fontSize: '14px',
+      color: 'white',
+      letterSpacing: '1px',
+    },
+  },
+}
+>>>>>>> Stashed changes
 
 const styles = {
   backgroundContainer: {
@@ -27,16 +56,20 @@ const styles = {
     gridArea: '1/1',
     position: 'relative',
   },
-  boxStyle: {
-    width: '130%',
+  boxStyleOuter: {
+    width: '100%',
     height: '100%',
   },
-  boxStyleRow: {
-    width: '50%',
-    height: '50%',
+  boxStyleInner: {
+    padding: '0 10vw 0 10vw',
+  },
+  buttonStyle: {
+    margin: '2vh 0 0 0',
+    backgroundColor: 'white',
   },
 }
 
+<<<<<<< Updated upstream
 const NotFoundPage = () => {
   return (
     <Layout spacingTop={false}>
@@ -107,5 +140,46 @@ const NotFoundPage = () => {
     </Layout>
   )
 }
+=======
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <Paper style={styles.backgroundContainer} alignItems="right">
+      <Grid
+        style={styles.boxStyleOuter}
+        container
+        color="white"
+        direction="row"
+        justify="flex-end"
+        alignItems="right"
+      >
+        <Grid
+          style={styles.boxStyleInner}
+          xs={12}
+          lg={6}
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <ThemeProvider theme={theme}>
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h6" align="center">
+              Pingvinen kunne ikke finne siden!
+            </Typography>
+            <Typography variant="body1" align="center" margin-bottom="2vh">
+              Men frykt ikke! Du kan hjelpe han å finne veien tilbake ved å
+              klikke knappen under.
+            </Typography>
+          </ThemeProvider>
+          <Grid style={styles.buttonStyle}>
+            <Button href="google.com">Tilbake til hovedsiden</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  </Layout>
+)
+>>>>>>> Stashed changes
 
 export default NotFoundPage
