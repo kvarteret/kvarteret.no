12099bac6d2f0c47533b3431a5175fbe67ca569d
TheAzack9 <TheAzack9@users.noreply.github.com>
2021-12-01 08:17:47 +0100

    feat: CI/CD blifrivillig

---
+++ apps/blifrivillig/next.config.published.js
@@ -0,0 +1,17 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+module.exports = withPlugins([withImages], {
+  webpack: (config) => {
+    // custom webpack config
+    return config;
+  },
+  images: {
+    domains: ['cms.kvarteret.no'],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['no', 'en'],
+    defaultLocale: 'no'
+  }
+});
