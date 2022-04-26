4f73076ae489795af7f8a234848dfde163929b41
Robin Grundvåg <robin@cavachi.be>
2022-04-26 18:45:38 +0200

    fix: rendering double

--- packages/dak-components/lib/components/Link.js
+++ packages/dak-components/lib/components/Link.js
@@ -4,17 +4,9 @@ import NextLink from "next/link";
 const Link = ({ href, target, rel, children, locale }) => {
   if (!href) return <>{children}</>;
   if(href.startsWith("http")) {
-    const childrenWithProps = React.Children.map(children, child => {
-      // Checking isValidElement is the safe way and avoids a typescript
-      // error too.
-      if (React.isValidElement(child)) {
-        return React.cloneElement(child, { href, target, rel, children, locale });
-      }
-      return child;
-    });
     return (
         <>
-          {childrenWithProps}
+          {React.cloneElement(children, { href, target, rel, children, locale })}
         </>
     );
   }
