b4cd881887ba107aa9a66ad048fceda36e5b9ab1
Robin Grundvåg <robin@cavachi.be>
2022-04-27 11:55:27 +0200

    fix: header buttons being broke

--- packages/dak-components/lib/components/layout/Navigation.js
+++ packages/dak-components/lib/components/layout/Navigation.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Link } from "../Link";
 
 const NavigationItem = ({ navItem, removeOpenMenus }) => {
@@ -8,8 +8,8 @@ const NavigationItem = ({ navItem, removeOpenMenus }) => {
   const isButton = navItem.isButton ? " button" : "";
 
   let url = navItem.url || "#";
-  if(isMulti) {
-    url = "#"
+  if (isMulti) {
+    url = "#";
   }
 
   const [timer, setTimer] = useState();
@@ -17,67 +17,74 @@ const NavigationItem = ({ navItem, removeOpenMenus }) => {
   const mouseClick = () => {
     removeOpenMenus();
     const item = document.getElementById(navItem.title);
-    if(!item) return;
+    if (!item) return;
     item.style.display = "flex";
-  }
+  };
 
   const mouseOver = () => {
-    setTimer(setTimeout(() => {
-      mouseClick();
-    }, 300))
+    setTimer(
+      setTimeout(() => {
+        mouseClick();
+      }, 300)
+    );
   };
 
   const mouseLeave = () => {
     clearTimeout(timer);
     setTimer(null);
-  }
+  };
 
-  let additionalParams = {}
-  if(url.startsWith("http")) {
-    additionalParams = { target: "_blank", rel: "noopener noreferrer"};
+  let additionalParams = {};
+  if (url.startsWith("http")) {
+    additionalParams = { target: "_blank", rel: "noopener noreferrer" };
   }
 
   return (
-    <Link href={url}>
-      <a {...additionalParams}>   
-        <div className={"container" + isButton} onMouseOver={mouseOver} onClick={mouseClick} onMouseLeave={mouseLeave}>
-          {navItem.title}
+    <div className="wrapper">
+      <Link href={url}>
+        <a key={url} {...additionalParams}>
+          <div
+            className={"container" + isButton}
+            onMouseOver={mouseOver}
+            onClick={mouseClick}
+            onMouseLeave={mouseLeave}
+          >
+            {navItem.title}
+          </div>
+        </a>
+      </Link>
+      <style jsx>
+        {`
+          .container {
+            padding: 8px 30px;
+            color: white;
+            font-weight: 400;
+            cursor: pointer;
+            user-select: none;
+            font-size: 16px;
+          }
+          .container:hover {
+            color: var(--primary);
+          }
 
-          <style jsx>
-            {`
-              .container {
-                padding: 8px 30px;
-                color: white;
-                font-weight: 400;
-                cursor: pointer;
-                user-select: none;
-                font-size: 16px;
-              }
-              .container:hover {
-                color: var(--primary);
-              }
-              
-              .button:hover {
-                color: white;
-              }
+          .button:hover {
+            color: white;
+          }
 
-              .button {
-                background-color: var(--primary-color);
-                user-select: none;
-              }
+          .button {
+            background-color: var(--primary-color);
+            user-select: none;
+          }
 
-
-              .button:hover {
-                background-color: #f85b5b;
-              }
-              .button:active {
-                background-color: #f23b3b;
-              }
-            `}
-          </style>
-        </div>
-      </a>
-    </Link>
+          .button:hover {
+            background-color: #f85b5b;
+          }
+          .button:active {
+            background-color: #f23b3b;
+          }
+        `}
+      </style>
+    </div>
   );
 };
 
@@ -85,7 +92,11 @@ const Navigation = ({ navItems, isRight, removeOpenMenus }) => {
   return (
     <div className="container">
       {navItems.map((x, index) => (
-        <NavigationItem key={index} navItem={x} removeOpenMenus={removeOpenMenus} />
+        <NavigationItem
+          key={index}
+          navItem={x}
+          removeOpenMenus={removeOpenMenus}
+        />
       ))}
       <style jsx>
         {`
