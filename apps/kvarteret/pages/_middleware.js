d9a33a3c3019285ef64da44082bc18fc27237935
Robin Grundvåg <robin@cavachi.be>
2022-04-29 14:56:56 +0200

    fix: Temp quick fix for redirect

--- apps/kvarteret/pages/_middleware.js
+++ apps/kvarteret/pages/_middleware.js
@@ -1,5 +1,4 @@
-import { getRedirectBySlugCached } from 'dak-components/lib/cms/queries/redirects';
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl
 
@@ -9,3 +8,60 @@ export async function middleware(req, ev) {
     }
     return NextResponse.next()
 }
+import NodeCache from "node-cache";
+import {differenceInMinutes} from "date-fns"
+const redirectCache = new NodeCache();
+
+async function upstash({
+  url,
+  token,
+  ...init
+}) {
+  const res = await fetch(url, {
+    ...init,
+    headers: {
+      authorization: `Bearer ${token}`,
+      ...init.headers,
+    },
+  })
+
+  const data = res.headers.get('Content-Type').includes('application/json')
+    ? await res.json()
+    : await res.text()
+
+  if (res.ok) {
+    return data
+  } else {
+    throw new Error(
+      `Upstash failed with (${res.status}): ${
+        typeof data === 'string' ? data : JSON.stringify(data, null, 2)
+      }`
+    )
+  }
+}
+
+const getAllRedirects = async () => {
+  const {data: redirects} = await upstash({
+    token: "ThisIsTestToken",
+    url: 'https://cms.kvarteret.no/items/redirects',
+  })
+  redirectCache.set("redirects", {time: new Date(), redirects: redirects});
+
+  return redirects;
+};
+
+const getAllRedirectsCached = async () => {
+  let {redirects, time} = redirectCache.get("redirects") || {};
+
+  if (differenceInMinutes(new Date(), time) <= 60) {
+    return redirects;
+  }
+  
+  return await getAllRedirects();
+};
+
+async function getRedirectBySlugCached(slug) {
+  const redirects = await getAllRedirectsCached();
+  return redirects.find((x) => x.slug === slug);
+}
+
--- packages/dak-components/lib/cms/queries/redirects.js
+++ packages/dak-components/lib/cms/queries/redirects.js
@@ -1,25 +1,43 @@
-import { gql } from "@apollo/client";
-import cmsClient from "../cmsClient";
 import NodeCache from "node-cache";
 import {differenceInMinutes} from "date-fns"
 const redirectCache = new NodeCache();
 
+async function upstash({
+  url,
+  token,
+  ...init
+}) {
+  const res = await fetch(url, {
+    ...init,
+    headers: {
+      authorization: `Bearer ${token}`,
+      ...init.headers,
+    },
+  })
+
+  const data = res.headers.get('Content-Type').includes('application/json')
+    ? await res.json()
+    : await res.text()
+
+  if (res.ok) {
+    return data
+  } else {
+    throw new Error(
+      `Upstash failed with (${res.status}): ${
+        typeof data === 'string' ? data : JSON.stringify(data, null, 2)
+      }`
+    )
+  }
+}
+
 const getAllRedirects = async () => {
-  const { data } = await cmsClient.query({
-    query: gql`
-      query GetAllRedirects {
-        redirects {
-          slug
-          permanent
-          destination
-        }
-      }
-    `,
-  });
+  const {data: redirects} = await upstash({
+    token: "ThisIsTestToken",
+    url: 'https://cms.kvarteret.no/items/redirects',
+  })
+  redirectCache.set("redirects", {time: new Date(), redirects: redirects});
 
-  redirectCache.set("redirects", {time: new Date(), redirects: data.redirects});
-
-  return data.redirects;
+  return redirects;
 };
 
 const getAllRedirectsCached = async () => {
