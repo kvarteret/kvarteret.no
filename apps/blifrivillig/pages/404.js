286f22ba67d60e71587449745fc7442ac1cf67cd
Robin Grundvåg <robin@cavachi.be>
2022-05-08 13:25:30 +0200

    fix: Dockerfile.blifrivillig

--- apps/blifrivillig/pages/404.js
+++ apps/blifrivillig/pages/404.js
@@ -1,5 +1,6 @@
 import { Custom404 } from "dak-components";
 import fetchLayoutData from "dak-components/lib/cms/layout";
+import { getTranslationsData } from "dak-components/lib/components/TranslatedField";
 
 export async function getStaticProps(context) {
     const layout = await fetchLayoutData(context.locale);
@@ -7,6 +8,7 @@ export async function getStaticProps(context) {
     return {
       props: {
         layout: layout,
+        translations: await getTranslationsData(context.locale, ["go-back", "404-title", "404-subtitle", "404-text"]),
       },
       revalidate: 1,
     };
--- apps/blifrivillig/pages/_app.js
+++ apps/blifrivillig/pages/_app.js
@@ -8,6 +8,7 @@ import createEmotionCache from "../src/createEmotionCache";
 import "../styles/globals.css";
 import "../public/fonts/fonts.css";
 import { Layout } from "dak-components";
+import { TranslationContext } from "dak-components/lib/components/TranslatedField";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -18,24 +19,29 @@ export default function MyApp(props) {
   const metadata = pageProps?.metadata;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Layout data={pageProps.layout}>
-        <Head>
-          {metadata && 
-            <>
-              <title>{metadata.title}</title>
-              <meta name="description" content={metadata.description} />
-            </>
-          }
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Layout>
-    </CacheProvider>
+    <TranslationContext.Provider value={pageProps.translations}>
+      <CacheProvider value={emotionCache}>
+        <Layout data={pageProps.layout}>
+          <Head>
+            {metadata && (
+              <>
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+              </>
+            )}
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Layout>
+      </CacheProvider>
+    </TranslationContext.Provider>
   );
 }
 
