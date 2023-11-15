6067309b7f5771d1002ef8d20a0a2a7888e497aa
Mathias Haugsbø <mathiash98@gmail.com>
2023-11-15 18:52:39 +0100

    Convert Carousel to tsx

--- apps/kvarteret/components/Carousel.tsx
+++ apps/kvarteret/components/Carousel.tsx
@@ -15,22 +15,29 @@ import { useEffect, useState } from "react";
 import Vibrant from "node-vibrant";
 
 const CarouselItem = ({ item }) => {
-
   // const { data, loading, error } = usePalette(`https://cms.kvarteret.no/assets/${imageId}`)
   const imageId = item.header.id;
   const [vibrancy, setVibrancy] = useState();
   useEffect(() => {
     const test = async () => {
       if (!Vibrant) return;
-      Vibrant.from(`https://cms.kvarteret.no/assets/${imageId}?width=5&height=5`).quality(1).getPalette().then(palette => {
-        setVibrancy(palette);
-      });
-    }
+      Vibrant.from(
+        `https://cms.kvarteret.no/assets/${imageId}?width=5&height=5`
+      )
+        .quality(1)
+        .getPalette()
+        .then((palette) => {
+          setVibrancy(palette);
+        });
+    };
 
     test();
-  }, [setVibrancy, imageId])
+  }, [setVibrancy, imageId]);
 
-  const link = (item?.navigation?.type === "page" ? item?.navigation?.page_2?.slug : item?.navigation?.url) ?? ""
+  const link =
+    (item?.navigation?.type === "page"
+      ? item?.navigation?.page_2?.slug
+      : item?.navigation?.url) ?? "";
 
   console.debug(link);
 
@@ -47,37 +54,38 @@ const CarouselItem = ({ item }) => {
           />
           <div className="content">
             <div className="title">{item.translations[0]?.title}</div>
-            <div className="description">{item.translations[0]?.description}</div>
+            <div className="description">
+              {item.translations[0]?.description}
+            </div>
           </div>
         </div>
       </a>
       <style jsx>
         {`
-        .container {
+          .container {
             height: 600px;
             width: 100vw;
-
-        }
+          }
           .image-container {
             height: 600px;
             position: relative;
             width: 100vw;
           }
-            .image-container:after {
-              content: "\A";
-              position: absolute;
-              width: 100%;
-              height: 100%;
-              top: 0;
-              left: 0;
-              background: rgba(0, 0, 0, 0);
-              transition: all 1s;
-              -webkit-transition: all 1s;
-              width : 100%;
-              height: 600px;
-            }
+          .image-container:after {
+            content: "\A";
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            background: rgba(0, 0, 0, 0);
+            transition: all 1s;
+            -webkit-transition: all 1s;
+            width: 100%;
+            height: 600px;
+          }
           .content {
-            left:50%;
+            left: 50%;
             top: 50%;
             transform: translate(-50%, -50%);
             position: absolute;
@@ -91,10 +99,17 @@ const CarouselItem = ({ item }) => {
             flex-direction: column;
             cursor: default;
           }
-          .title, .description {
+          .title,
+          .description {
             color: white;
-            font-size: max(40px, calc(60px - ${(item.translations[0]?.title?.length ?? 0) / 2.0}px));
-            /* text-shadow: 0 0 2px ${vibrancy?.Muted?.getHex() ?? "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ?? "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ?? "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ?? "black"}; */
+            font-size: max(
+              40px,
+              calc(60px - ${(item.translations[0]?.title?.length ?? 0) / 2.0}px)
+            );
+            /* text-shadow: 0 0 2px ${vibrancy?.Muted?.getHex() ??
+            "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ??
+            "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ??
+            "black"}, 0 0 2px ${vibrancy?.Muted?.getHex() ?? "black"}; */
             max-width: 1200px;
             width: calc(100% - 200px);
             overflow-x: hidden;
@@ -105,18 +120,23 @@ const CarouselItem = ({ item }) => {
             font-size: 24px;
             white-space: pre-wrap;
           }
-          
-            @media (max-width: 768px) {
-              .title {
-                font-size: max(26px, calc(40px - ${(item.translations[0]?.title?.length ?? 0) / 2.0}px));
-                text-overflow: initial;
-                overflow-x: initial;
-                overflow-wrap: break-word;
-              }
-              .description {
-                font-size: 20px;
-                width: 230px;
-              }
+
+          @media (max-width: 768px) {
+            .title {
+              font-size: max(
+                26px,
+                calc(
+                  40px - ${(item.translations[0]?.title?.length ?? 0) / 2.0}px
+                )
+              );
+              text-overflow: initial;
+              overflow-x: initial;
+              overflow-wrap: break-word;
+            }
+            .description {
+              font-size: 20px;
+              width: 230px;
+            }
           }
         `}
       </style>
@@ -124,7 +144,12 @@ const CarouselItem = ({ item }) => {
   );
 };
 
-const Carousel = ({ carouselItems, component }) => {
+interface CarouselProps {
+  carouselItems: any[];
+  component?: ({ item }: { item: any }) => JSX.Element;
+}
+
+const Carousel = ({ carouselItems, component }: CarouselProps) => {
   return (
     <div className="carousel">
       <Swiper
@@ -137,15 +162,13 @@ const Carousel = ({ carouselItems, component }) => {
         autoplay={{
           delay: 5000,
           disableOnInteraction: false,
-          pauseOnMouseEnter: true
+          pauseOnMouseEnter: true,
         }}
         pagination={{ clickable: true }}
       >
         {carouselItems.map((x, i) => (
           <SwiperSlide key={i}>
-            {component && component({ item: x })
-              || <CarouselItem item={x} />
-            }
+            {component ? component({ item: x }) : <CarouselItem item={x} />}
           </SwiperSlide>
         ))}
       </Swiper>
@@ -160,6 +183,5 @@ const Carousel = ({ carouselItems, component }) => {
     </div>
   );
 };
-
 
 export { Carousel };
